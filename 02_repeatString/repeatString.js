bee9620df703e5ea71e614496087b8418c49450e
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-01-30 18:43:56 +1100

    feat: complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, counts) {
+    if (counts < 0) {
+        return "ERROR";
+    }
+    
+    output = "";
+    for (let i = 0; i < counts; i++) {
+        output = output + word;
+    }
+    return output
 };
 
 // Do not edit below this line
