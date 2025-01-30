1db42f34a43f241d21d72f9c3023efa0788eea64
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-01-30 19:19:40 +1100

    feat: complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || 
+        !Number.isInteger(b) ||
+        a < 0 ||
+        b < 0) {return "ERROR"} 
+    if (a > b) {[b, a] = [a, b]}
+    let sum = 0;
+    while (a <= b) {
+        sum += a;
+        a++;
+    }
+    return sum
 };
 
 // Do not edit below this line
