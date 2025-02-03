da922e6b9165a45f3fc81ca9fa575402bf436eb1
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-02-04 10:00:37 +1100

    feat: complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,9 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.sort((aPerson, bPerson) => {
+        const aAge = (aPerson.yearOfDeath?aPerson.yearOfDeath:new Date().getFullYear()) - aPerson.yearOfBirth;
+        const bAge = (bPerson.yearOfDeath?bPerson.yearOfDeath:new Date().getFullYear()) - bPerson.yearOfBirth;
+        return bAge - aAge
+    })[0]
 
 };
 
