d24a65a7152baa95c820c6f2818f3e6c2d323db6
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-02-04 09:37:26 +1100

    feat: complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS"
+    } else if (+a === 1) {
+        return 1
+    } else if (+a === 0) {
+        return 0
+    }
+    return fibonacci(a-1) + fibonacci(a-2)
 };
 
 // Do not edit below this line
