dba792574b3720ea69e379310d927728ae795760
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-02-04 09:29:30 +1100

    feat: complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const cleanString = input.replaceAll(",","").replaceAll(" ", "").replaceAll("!","").replaceAll(".","");
+    const arr = cleanString.split("").map((a) => a.toLowerCase());
+    const reverseArr = [...arr].reverse();
+    return arr.toString() === reverseArr.toString()
 };
 
 // Do not edit below this line
